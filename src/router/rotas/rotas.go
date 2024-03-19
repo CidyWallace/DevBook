c16793cefcdad29d968bc19c07524fed48373511
CidyWallace <cidywallace12@gmail.com>
2024-03-18 22:12:11 -0300

    Router Config

---
+++ src/router/rotas/rotas.go
@@ -0,0 +1,26 @@
+package rotas
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Rota representa representa todas as rotas da API
+type Rota struct {
+	URI                string
+	Metodo             string
+	Funcao             func(w http.ResponseWriter, r *http.Request)
+	RequerAutenticacao bool
+}
+
+// Configurar coloca todas as rotas dentro do router
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+
+	for _, rotas := range rotas {
+		r.HandleFunc(rotas.URI, rotas.Funcao).Methods(rotas.Metodo)
+	}
+
+	return r
+}
