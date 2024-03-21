b3c5ae52acb1e045ef9d18c47e8b168df7bc690f
CidyWallace <cidywallace12@gmail.com>
2024-03-20 23:41:54 -0300

    Adicionando o Deletar usuários

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -144,5 +144,28 @@ func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 
 // DeletarUsuario apaga um usuário do banco de dados
 func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Deletando Usuário!"))
+	parametro := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametro["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+
+	if erro = repositorio.Deletar(ID); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
+
 }
