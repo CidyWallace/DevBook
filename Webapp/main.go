8b7eb98f98d76e5cde91bada80aa3cdd9cc52b48
CidyWallace <cidywallace12@gmail.com>
2024-04-13 10:56:48 -0300

    Change main

--- Webapp/main.go
+++ Webapp/main.go
@@ -12,6 +12,6 @@ func main() {
 	utils.CarregarTemplates()
 	r := router.Gerar()
 
-	fmt.Println("Rodando WebApp")
+	fmt.Println("Rodando WebApp na porta: 3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
